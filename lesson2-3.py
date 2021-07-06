31156997760f0f6f1c354b1751edb0d56079881f
Vivianliao009204 <86942135+Vivianliao009204@users.noreply.github.com>
2021-07-06 12:22:25 +0800

    Add files via upload

---
+++ lesson2-3.py
@@ -0,0 +1,11 @@
+x=input('輸入數學成績')
+x=int(x)
+y=input('輸入英文成績')
+y=int(y)
+if x>=90 and y>=90 :
+    print('恭喜你得到校長獎金')
+elif x==100 or y==100 :
+    print('恭喜你得到校長獎金')
+else:    
+    print('好可惜,下次請加油')
+    
